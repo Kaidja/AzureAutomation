bb30888d7ed51b2b3007f06fb89ee5e190db54a4
Kaido Järvemets <me@kaidojarvemets.ee>
2021-07-23 15:06:31 +0300

    Update Get-SAMACLPermissionsV2.ps1

--- Get-SAMACLPermissionsV2.ps1
+++ Get-SAMACLPermissionsV2.ps1
@@ -1,6 +1,11 @@
 $SAMACL = (Get-Acl -Path "C:\Windows\System32\config\SAM").Access | Where-Object {$PSITEM.IdentityReference -eq "BUILTIN\Users"}
-If($SAMACL.FileSystemRights.ToString().Contains("Read")){
-    Write-Host -Object "Vulnerable"
+If($SAMACL){
+    If($SAMACL.FileSystemRights.ToString().Contains("Read")){
+        Write-Host -Object "Vulnerable"
+    }
+    Else{
+        Write-Host -Object "NOTVulnerable"
+    }
 }
 Else{
     Write-Host -Object "NOTVulnerable"
